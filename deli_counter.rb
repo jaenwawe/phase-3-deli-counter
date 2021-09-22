83f2343d0405df1e91fcf4a7449dd8e765aee941
jaenwawe <nwawe.jae@gmail.com>
2021-09-21 19:16:36 -0500

    commit tree 869d3a7d857bd7529cb2d5276bb75b5b98364c57

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,20 @@
 # Write your code here.
+
+def line name
+    katz_deli = []
+    if  katz_deli.size == 0  
+        puts "The line is currently empty."
+    puts "#{name } #{take_a_number} #{katz_deli.size}"
+    end
+end 
+
+def take_a_number (katz_deli, name)
+    puts "#{name} is at #{katz_deli.size}"
+end
+
+def now_serving (katz_deli, name)
+    katz_deli = katz_deli.pop
+    if  katz_deli.size == puts "There is nobody waiting to be served!"
+    end
+
+end
